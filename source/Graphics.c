dbd296c1ec7fbf6202616190e86eb323c67c8fdc
MickaelFrey <mickael.frey@epfl.ch>
2017-12-19 22:41:09 +0100

    tests

--- source/Graphics.c
+++ source/Graphics.c
@@ -5,7 +5,11 @@
  * Configure the graphics settings for the MenuStart
  */
 void configure_MenuStart(){
-	//...
+	//Enable a proper RAM memory bank for sub engine
+	VRAM_C_CR = VRAM_ENABLE | VRAM_C_SUB_BG;
+
+	//Configure the sub engine in mode 5 (2D) and activate Backgrounds 2 and 0
+	REG_DISPCNT_SUB = MODE_0_2D | DISPLAY_BG0_ACTIVE;
 }
 
 /*
