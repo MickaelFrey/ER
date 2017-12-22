56db7b8b7ee3de8b3fabf1924e8c1d7dd2133880
MickaelFrey <mickael.frey@epfl.ch>
2017-12-22 17:31:30 +0100

    tests

--- build/background_room1.h
+++ build/background_room1.h
@@ -3,13 +3,13 @@
 
 //======================================================================
 //
-//	background_room1, 512x512@8, 
+//	background_room1, 512x384@8, 
 //	+ palette 256 entries, not compressed
-//	+ 261 tiles (t|f reduced) not compressed
-//	+ regular map (flat), not compressed, 64x64 
-//	Total size: 512 + 16704 + 8192 = 25408
+//	+ 1724 tiles (t|f reduced) not compressed
+//	+ regular map (flat), not compressed, 64x48 
+//	Total size: 512 + 110336 + 6144 = 116992
 //
-//	Time-stamp: 2017-12-22, 10:10:48
+//	Time-stamp: 2017-12-22, 17:28:08
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,11 +18,11 @@
 #ifndef GRIT_BACKGROUND_ROOM1_H
 #define GRIT_BACKGROUND_ROOM1_H
 
-#define background_room1TilesLen 16704
-extern const unsigned int background_room1Tiles[4176];
+#define background_room1TilesLen 110336
+extern const unsigned int background_room1Tiles[27584];
 
-#define background_room1MapLen 8192
-extern const unsigned short background_room1Map[4096];
+#define background_room1MapLen 6144
+extern const unsigned short background_room1Map[3072];
 
 #define background_room1PalLen 512
 extern const unsigned short background_room1Pal[256];
